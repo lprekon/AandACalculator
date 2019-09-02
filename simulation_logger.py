53711978a98e2c83751ca3c10f33f31955912fbb
John O'kane <jtommyok8@gmail.com>
2019-09-02 16:41:43 -0400

    added 'complete' statement

--- simulation_logger.py
+++ simulation_logger.py
@@ -2,4 +2,5 @@ def simulation_logger(sim_step):
 	def wrapper(*args, **kwargs):
 		print("[*] Executing {}\nargs: {}\nkwargs: {}".format(sim_step.__name__, args, kwargs))
 		sim_step(*args, **kwargs)
+		print("[+] Complete")
 	return wrapper
